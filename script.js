820de11c39a21613b866d33aad2fa3e96d9ed236
Nick Meinhold <nick@enspyr.co>
2025-10-16 22:32:36 +1100

    Create buttons with js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const Gameboard = (function () {
 
   return {};
 })();
+
+let gameBaordDiv = document.getElementById("gameBoard");
+
+for (let i = 0; i < 9; i++) {
+  let gridItemButton = document.createElement("button");
+  gridItemButton.className = "grid-item";
+
+  gridItemButton.addEventListener("click", () => {
+    console.log(i);
+  });
+
+  gameBaordDiv.appendChild(gridItemButton);
+}
